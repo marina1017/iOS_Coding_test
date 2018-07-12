0a1fc6a2b8623651a5bcade16c5443df8a87c690
marina1017 <mari10172001@yahoo.co.jp>
2018-07-12 22:24:58 +0900

    launch image導入

--- iOS_Coding_test/AppDelegate.swift
+++ iOS_Coding_test/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
 
-        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = UIWindow()
         self.window?.backgroundColor = .white
 
         // ページを格納する配列
