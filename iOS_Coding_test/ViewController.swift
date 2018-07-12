fc9f6b609077e29f7b9b0951dce5b9854fa1c5f1
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-07-12 19:53:03 +0900

    環境を整える

---
+++ iOS_Coding_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Coding_test
+//
+//  Created by nakagawa on 2018/07/12.
+//  Copyright © 2018年 nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
